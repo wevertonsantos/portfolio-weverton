49e3f2cf51f406b05ef2e9159c3096916841a621
Weverton dos Santos <wever-santos@hotmail.com>
2023-03-31 16:17:19 -0300

    base do projeto

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <Cabecalho/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/index.js
@@ -0,0 +1,16 @@
+import 'Cabecalho.css'
+
+const Cabecalho = () => {
+	return(
+		<header className="cabecalho">
+			<h1>WS</h1>
+			<div>
+				<h3>Principal</h3>
+				<h3>Habilidades</h3>
+				<h3>Projetos</h3>
+			</div>
+		</header>
+	)
+}
+
+export default Cabecalho
