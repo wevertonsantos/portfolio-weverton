614e16be24dd9eed8880287bf26fb13016f9e276
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-01 11:49:30 -0300

    adicionando estilos e fonte

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Cabecalho from "./componentes/Cabecalho"
+import Apresentacao from "./componentes/Apresentacao"
+
 function App() {
   return (
     <div className="App">
       <Cabecalho/>
+      <Apresentacao/>
     </div>
   );
 }
