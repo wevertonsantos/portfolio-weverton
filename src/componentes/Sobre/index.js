e56792927f725038136c51c4d695cb3801b844a9
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-01 18:07:35 -0300

    Adicionando sobre

---
+++ src/componentes/Sobre/index.js
@@ -0,0 +1,16 @@
+import './Sobre.css'
+
+const Sobre = () => {
+    return(
+        <section className='sobre'>
+            <h1>Sobre</h1>
+            <p> Sou apaixonado por criar. 
+                Formado em análise e desenvolvimento de sistemas,
+                faço cursos profissionalizantes de tecnologia da informação
+                e tenho mais de 6 meses de experiência em desenvolvimento front-end.
+            </p>
+        </section>
+    )
+}
+
+export default Sobre
