39546863550fcee1a2358d86a1607dc1f5145d54
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-01 18:06:34 -0300

    Adicionando novos componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Cabecalho from "./componentes/Cabecalho"
 import Apresentacao from "./componentes/Apresentacao"
+import Tecnologias from "./componentes/Tecnologias"
+import Sobre from "./componentes/Sobre"
 
 function App() {
   return (
     <div className="App">
       <Cabecalho/>
       <Apresentacao/>
+      <Sobre/>
+      <Tecnologias/>
     </div>
   );
 }
