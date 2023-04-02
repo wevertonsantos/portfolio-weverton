890089e334796206f6fc989c49ff3260295f95f5
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-02 09:16:28 -0300

    ao scrollar o cabeçalho muda de cor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React, { useEffect, useState } from "react"
 import Cabecalho from "./componentes/Cabecalho"
 import Apresentacao from "./componentes/Apresentacao"
 import Sobre from "./componentes/Sobre"
 import Tecnologias from "./componentes/Tecnologias"
 
 function App() {
+
+  const [ativaCor, setAtivaCor] = useState(false)
+
+  useEffect(function(){
+    function posicaoScroll(){
+      if(window.scrollY > 10){
+        setAtivaCor(true)
+      }else{
+        setAtivaCor(false)
+      }
+    }
+
+    window.addEventListener('scroll', posicaoScroll)
+  }, [])
+
   return (
     <div className="App">
-      <Cabecalho/>
+      <Cabecalho acao={ativaCor}/>
       <Apresentacao/>
       <Sobre/>
       <Tecnologias/>
