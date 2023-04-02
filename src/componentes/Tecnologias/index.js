bf8e2aa415cfba223016b4e280b0ec5a6e4486ad
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-02 08:15:46 -0300

    adicionando icones

---
+++ src/componentes/Tecnologias/index.js
@@ -0,0 +1,17 @@
+import './Tecnologias.css'
+
+const Tecnologias = () => {
+    return(
+        <section className="tecnologias">
+            <h1>Tecnologias</h1>
+            <div>
+                <h2><i class="fa-brands fa-css3-alt"></i></h2>
+                <h2><i class="fa-brands fa-html5"></i></h2>
+                <h2><i class="fa-brands fa-square-js"></i></h2>
+                <h2><i class="fa-brands fa-react"></i></h2>
+            </div>
+        </section>
+    )
+}
+
+export default Tecnologias
