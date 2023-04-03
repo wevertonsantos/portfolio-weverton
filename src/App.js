7133308f136f1b179491efcc15b0b68b34ff867e
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-03 16:18:57 -0300

    apresentação e mudando arquivo app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from "react"
 import Cabecalho from "./componentes/Cabecalho"
 import Apresentacao from "./componentes/Apresentacao"
 import Sobre from "./componentes/Sobre"
-import Tecnologias from "./componentes/Tecnologias"
+import Projetos from "./componentes/Projetos"
+import Habilidades from "./componentes/Habilidades"
 
 function App() {
 
   const [ativaCor, setAtivaCor] = useState(false)
-
-  useEffect(function(){
-    function posicaoScroll(){
+  useEffect(() => {
+    const posicaoScroll = () => {
       if(window.scrollY > 10){
         setAtivaCor(true)
       }else{
@@ -20,12 +20,14 @@ function App() {
     window.addEventListener('scroll', posicaoScroll)
   }, [])
 
+
   return (
     <div className="App">
       <Cabecalho acao={ativaCor}/>
       <Apresentacao/>
       <Sobre/>
-      <Tecnologias/>
+      <Habilidades/>
+      <Projetos/>
     </div>
   );
 }
