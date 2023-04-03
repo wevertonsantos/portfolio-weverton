fa5ff9291be08caa7df4cb18a89655f71b7d2c1c
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-03 16:19:47 -0300

    container projetos

---
+++ src/componentes/Projetos/index.js
@@ -0,0 +1,28 @@
+import './Projetos.css'
+
+const Projetos = () => {
+    return(
+        <section className='projetos' id="projetos">
+            <h1>Projetos</h1>
+
+            <div className='container'>
+                <a href='https://github.com/wevertonsantos/organo-reactjs'>
+                    <div>
+                    </div>
+                </a>
+                <a href='https://github.com/wevertonsantos/play-requisicoes'>
+                    <div>
+                    </div>
+                </a>
+                <a href='https://github.com/wevertonsantos/consultBooks'>
+                    <div>
+                    </div>
+                </a>
+            </div>
+
+            <a href='https://github.com/wevertonsantos?tab=repositories'><button>Ver mais</button></a>
+        </section>
+    )
+}
+
+export default Projetos
