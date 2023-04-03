2971302fc7c6f9efde9398b3de58bd8da192d4dc
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-03 16:19:08 -0300

    mudando links para projetos

--- src/componentes/Apresentacao/index.js
+++ src/componentes/Apresentacao/index.js
@@ -3,13 +3,13 @@ import imgProgramador from '../../imagens/programador.png'
 
 const Apresentacao = () => {
     return(
-        <section className="apresentacao">
+        <section className="apresentacao" id="inicio">
             <div>
                 <h2>Olá, eu sou</h2>
                 <h1>Weverton Santos</h1>
                 <p>Criações e experiência no desenvolvimento front-end</p>
-                <button>Download CV</button>
-                <button>Contrate-me</button>
+                <a href='https://drive.google.com/file/d/1q1pS-7i36ruh5_R11MPa6Gcwk2NhKL2I/view?usp=share_link'><button>Download CV</button></a>
+                <a href='https://www.linkedin.com/in/wevertonsantoss/'><button>Contrate-me</button></a>
             </div>
             <img src={imgProgramador}/>
         </section>
