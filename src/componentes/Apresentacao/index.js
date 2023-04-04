eb56a9c469e0f5698194d7afcd036d568abb6c71
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-04 15:10:02 -0300

    responsividade apresentação

--- src/componentes/Apresentacao/index.js
+++ src/componentes/Apresentacao/index.js
@@ -8,7 +8,7 @@ const Apresentacao = () => {
                 <h2>Olá, eu sou</h2>
                 <h1>Weverton Santos</h1>
                 <p>Criações e experiência no desenvolvimento front-end</p>
-                <a href='https://drive.google.com/file/d/1q1pS-7i36ruh5_R11MPa6Gcwk2NhKL2I/view?usp=share_link'><button>Download CV</button></a>
+                <a href='https://drive.google.com/file/d/1V9V1BS2JDyDpSpLX_M6seg3cMk7-_Oxi/view?usp=share_link'><button>Download CV</button></a>
                 <a href='https://www.linkedin.com/in/wevertonsantoss/'><button>Contrate-me</button></a>
             </div>
             <img src={imgProgramador}/>
