bfe2e7448c8c2a2dd6ebc16b705700072be26bf5
Weverton dos Santos <wever-santos@hotmail.com>
2023-05-21 14:33:07 -0300

    cv

--- src/componentes/Apresentacao/index.js
+++ src/componentes/Apresentacao/index.js
@@ -8,7 +8,7 @@ const Apresentacao = () => {
                 <h2>Olá, eu sou</h2>
                 <h1>Weverton Santos</h1>
                 <p>Criações e experiência no desenvolvimento web</p>
-                <a href='https://drive.google.com/file/d/1t2P52Y-nJXYfKyQln6iduQUGjC12dE38/view?usp=sharing'><button>Download CV</button></a>
+                <a href='https://drive.google.com/file/d/19d_FYKV_lXl_lFera6LUQw84P-Er6kec/view?usp=sharing'><button>Download CV</button></a>
                 <a href='https://www.linkedin.com/in/wevertonsantoss/'><button>Contrate-me</button></a>
             </div>
             <img src={imgProgramador}/>
