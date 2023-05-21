b63e1726abfbc472e99723e1b8734110bfa1cd08
Weverton dos Santos <wever-santos@hotmail.com>
2023-05-21 13:29:11 -0300

    alterações no portfólio

--- src/componentes/Sobre/index.js
+++ src/componentes/Sobre/index.js
@@ -5,9 +5,9 @@ const Sobre = () => {
         <section className='sobre' id="sobre">
             <h1>Sobre</h1>
             <p> Sou apaixonado por criar. 
-                Sempre fui bastante criativo, gosto de colocar as coisas que sei em prática, desde criação
-                de jogos online até de sites. Depois do ensino médio em 2019 já quando estava prestes a entrar na faculdade
-                me aprofundei em tecnologia e desde então é onde eu coloco minha criatividade em prática, com o meu mergulho em desenvolvimento web.
+                Sempre gostei bastante de tecnologia, gosto de colocar as coisas que sei em prática, desde criação
+                de jogos online até sites. Depois do ensino médio em 2019 já quando estava prestes a entrar na faculdade
+                me aprofundei em tecnologia e desde então é onde eu coloco minha criatividade e com isso mergulhei no desenvolvimento web.
             </p>
         </section>
     )
